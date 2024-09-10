07902cbbc1c71925cd2f093186b41be0bfc107ce
Janine Escobillo <janineescobillo@hotmail.com>
2024-09-10 21:25:47 +0200

    adds signout button

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,21 +1,33 @@
+
+import SignOutButton from "@/components/SignOutButton"
+import useAuth from "@/lib/useAuth"
 import Link from "next/link"
 
+
+
+
 const Navbar = () => {
+
+  const user = useAuth()
+
+
+
   return (
-    <div className="">
-      <ul className="flex justify-center  text-white text-lg font-serif py-8 px-4 gap-x-3 bg-blue-950 font-bold">
+    <div className="bg-blue-950">
+      <ul className="flex rounded justify-center text-white text-lg font-serif py-7 px-3 gap-x-4 bg-blue-950 font-bold">
         <Link href="/" >
-          Home 
+          Home
         </Link>
         <Link href="/create" >
-          Create 
+          Create
         </Link>
-        <Link href="Login">
-          Log in 
-        </Link>
-        {/* <Link href="/threads" >
-          Threads
-        </Link> */}
+        
+        {user && <Link href="/threads">Threads</Link>}
+        {user ? (
+          <SignOutButton />
+        ) : (
+          <Link href="Login">Log in</Link>
+        )}
       </ul>
     </div>
   )
