4cff9eedbda228723f51c649e3f735132c3fff7c
Janine Escobillo <janineescobillo@hotmail.com>
2024-09-11 11:26:29 +0200

    adds useAuth

--- components/ListThreads.tsx
+++ components/ListThreads.tsx
@@ -1,4 +1,5 @@
 
+import useAuth from '@/lib/useAuth';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
@@ -6,10 +7,16 @@ import React, { useEffect, useState } from 'react';
 function ListThreads() {
   const [threads, setThreads] = useState<Thread[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const user = useAuth()
 
   useEffect(() => {
     const savedThreads = localStorage.getItem('threads');
     if (savedThreads) {
+
+      const parsedThreads = JSON.parse(savedThreads).map((thread: Thread) => ({
+        ...thread,
+        locked: thread.locked || false
+      }))
       setThreads(JSON.parse(savedThreads));
     } else {
       setError('Inga trådar hittades i localStorage.');
@@ -24,28 +31,51 @@ function ListThreads() {
     console.log('Updated threads:', updatedThreads);
   };
 
+  const lockThread = (id: number) => {
+    const updatedThreads = threads.map(thread => thread.id === id ? {
+      ...thread,
+      locked: true
+    } : thread)
+
+    setThreads(updatedThreads);
+    localStorage.setItem('threads', JSON.stringify(updatedThreads));
+  }
+
+  const unlockThread = (id: number) => {
+    const updatedThreads = threads.map(thread => thread.id === id ? {
+      ...thread,
+      locked: false,
+
+    } : thread)
+    setThreads(updatedThreads);
+    localStorage.setItem('threads', JSON.stringify(updatedThreads));
+  }
+
   return (
-<main className="border-2 flex items-center justify-center  bg-blue-300  rounded p-10 shadow-xl">
+    <main className="border-2 flex items-center justify-center  bg-blue-300  rounded p-10 shadow-xl">
       <div className="">
         <div className=" text-black flex items-center justify-center bg-blue-300 flex-col">
-              {error ? (
-                <p className="text-red-500 text-xs italic">{error}</p>
-              ) : (
-                threads.map(thread => (
-                  <div key={thread.id} className=" justify-between rounded items-center mb-4 p-4 border border-white w-[600px]">
-                    <Link href={`/threads/${thread.id}`}>{thread.title}</Link>
-                    <p className="mt-2 text-sm">{thread.category}</p>
-                    <p className="text-sm text-slate-500">{new Date(thread.creationDate).toLocaleString()}</p>
-                    {/* <p className="mt-2 text-sm">{thread.description}</p> */}
-                    <button
-                      className="mt-2 bg-red-500 text-sm text-white px-3  rounded "
-                      onClick={() => deleteThread(thread.id)}
-                    >
-                      Delete
-                    </button>
+          {error ? (
+            <p className="text-red-500 text-xs italic">{error}</p>
+          ) : (
+            threads.map(thread => (
+              <div key={thread.id} className=" justify-between rounded items-center mb-4 p-4 border border-white w-[600px]">
+                <Link href={`/threads/${thread.id}`}>{thread.title}</Link>
+                <p className="mt-2 text-sm">{thread.category}</p>
+                <p className="text-sm text-slate-500">{new Date(thread.creationDate).toLocaleString()}</p>
+                <p className="mt-2 text-sm">{thread.description}</p>
+                {user && ( // Conditionally render the buttons based on user authentication status
+                  <div>
+                    <button onClick={() => lockThread(thread.id)}>Lock</button>
+                    <button onClick={() => unlockThread(thread.id)}>Unlock</button>
+                    <button onClick={() => deleteThread(thread.id)}> Delete</button>
                   </div>
-                ))
-              )}
+                )}
+                
+
+              </div>
+            ))
+          )}
         </div>
       </div>
     </main>
---
+++ lib/useAuth.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "@/firebase/config";
+
+
+const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return user;
+};
+
+export default useAuth;
