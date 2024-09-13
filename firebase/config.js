5890aaa8bb4e056eb4de5d49b59f34e796b57e6f
Janine Escobillo <janineescobillo@hotmail.com>
2024-09-14 01:06:27 +0200

    cleanup config

--- firebase/config.js
+++ firebase/config.js
@@ -1,27 +1,24 @@
-'use client'
-
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getFireStore } from "firebase/firestore";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY, 
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID, 
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET, 
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID, 
-  appId: process.env.NEXT_PUBLIC_APP_ID, 
- 
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
-// Initialize Firebase
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-const db = getFireStore(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const db = getFirestore(app);
 const auth = getAuth(app);
-// const googleProvider = new GoogleAuthProvider();
 const storage = getStorage(app);
 
-export { db, auth, storage, app};
+
+export { db, auth, app, storage};
