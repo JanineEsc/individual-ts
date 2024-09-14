5453ad5d5ad6a691842a7c28ae413741a0c2606d
Janine Escobillo <janineescobillo@hotmail.com>
2024-09-14 09:28:53 +0200

    clean up

--- app/threads/[id]/page.tsx
+++ app/threads/[id]/page.tsx
@@ -15,7 +15,7 @@ const ThreadDetail = () => {
   const [comment, setComment] = useState<string>("");
   const [checkAnswer, setCheckAnswer] = useState<string | null>(null)
   const [reply, setReply] = useState<{ [key: string]: string }>({});
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true); 
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
   const user = useAuth();
 
 
@@ -106,9 +106,27 @@ const ThreadDetail = () => {
     }
   };
 
-  const toggleCheck = (commentId: string) => {
-    setCheckAnswer(preId => (preId === commentId ? null : commentId))
-  }
+  const toggleCheck = async (commentId: string) => {
+    if (!thread) return;
+
+    const updatedComments = thread.comments.map((comment) => {
+      if (comment.id === commentId) {
+        return { ...comment, isAnswer: !comment.isAnswer };
+      } else {
+        return { ...comment, isAnswer: false };
+      }
+    });
+
+    const updatedThread = { ...thread, comments: updatedComments };
+    setThread(updatedThread);
+
+    try {
+      await updateDoc(doc(db, 'threads', thread.id), { comments: updatedComments });
+      setCheckAnswer((prev) => (prev === commentId ? null : commentId));
+    } catch (error) {
+      console.error('Error updating document: ', error);
+    }
+  };
 
   if (!thread) {
     return <p className="text-red-500 text-center text-8xl">Thread not found.</p>;
@@ -127,87 +145,87 @@ const ThreadDetail = () => {
                 {thread && new Date(thread.creationDate).toLocaleString()}
               </p>
 
-
-              {thread.locked ? (
-                <div className=" text-red-700  p-2 rounded mt-2">
+              {thread?.locked && (
+                <div className="text-red-700 p-2 rounded mt-2">
                   <p className="text-center font-bold text-sm">This thread is locked. You cannot comment or reply.</p>
                 </div>
-              ) : (
-                <>
-                  <hr className="mt-4" />
-                  <div className="my-4 p-9">
-                    {thread?.comments.map((comment) => (
-                      <div key={comment.id} className="mb-4">
-                        <p className="bg-slate-200 rounded p-3 text-teal-900 font-semibold">
-                          {comment.content}
-                          {checkAnswer === comment.id && <CheckIcon className="text-green-600 ml-2 float-right" />}
-                        </p>
-                        <p className="text-white text-sm flex p-1 justify-between ">
-                          {comment.creator.userName}
-                          {thread.category === 'QNA' && isLoggedIn && (
-                            <button
-                              className="flex items-center bg-gray-950 float right "
-                              onClick={() => toggleCheck(comment.id)}
-                            >
-                              {checkAnswer !== comment.id && <CheckIcon className="text-green-600" />}
-                              <p className="bg-gray-950">{checkAnswer === comment.id ? 'Remove answer' : 'Mark as answer'}</p>
-                            </button>
-                          )}
-                        </p>
-                        <p className="text-gray-400 text-xs">
-                          Replies: {comment.replies ? comment.replies.length : 0}
-                        </p>
-                        <p className="text-gray-400 text-xs">Likes: {comment.likes || 0}</p>
-
-                        {/* Replies */}
-                        <div className="ml-6 mt-2 border-l-2 pl-4 border-gray-300">
-                          {comment.replies &&
-                            comment.replies.map((reply) => (
-                              <div key={reply.id} className="ml-2 mt-2 bg-gray-100 rounded p-2 shadow-md">
-                                <p className="text-gray-900">{reply.content}</p>
-                                <p className="text-gray-500 text-xs">- {reply.creator.userName}</p>
-                              </div>
-                            ))}
-                        </div>
-
-                        {/* Reply textarea and button */}
-                        <div className="mt-3 ml-6">
-                          <textarea
-                            className="w-full p-2 bg-gray-800 text-white rounded-lg"
-                            placeholder="Write your reply..."
-                            value={reply[comment.id] || ""}
-                            onChange={(e) => setReply({ ...reply, [comment.id]: e.target.value })}
-                          />
-                          <button
-                            className="mt-2 bg-emerald-500 text-white px-3 py-1 rounded-md hover:bg-emerald-700"
-                            onClick={() => handleReply(comment.id)}
-                          >
-                            Reply
-                          </button>
-                        </div>
-                      </div>
-                    ))}
-                    <hr className="mt-7" />
-
-                    {/* Comment Input */}
-                    <div className="relative flex items-center right-5 mt-2 w-[600px]">
-                      <textarea
-                        className="text-white m-4 p-5 rounded-md w-[800px] h-[80px] border  bg-gray-800"
-                        placeholder="Write your comment..."
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
-                      />
-                      <button
-                        className="text-emerald-500 rounded-md px-5 w-13 h-[50px] hover:bg-emerald-800 bg-white absolute right-9"
-                        onClick={handleAddComment}
-                        title={!user ? "You must be logged in" : ""}
-                        >
-                        Comment
-                      </button>
-                    </div>
-                  </div>
-                </>
               )}
+
+              <hr className="mt-4" />
+              <div className="my-4 p-9">
+                {thread?.comments.map((comment) => (
+                  <div key={comment.id} className="mb-4">
+                    <p className="bg-slate-200 rounded p-3 text-teal-900 font-semibold">
+                      {comment.content}
+                      {comment.isAnswer && <CheckIcon className="text-green-600 ml-2 float-right" />}
+                    </p>
+                    <p className="text-white text-sm flex p-1 justify-between ">
+                      {comment.creator.userName}
+                      {thread.category === 'QNA' && isLoggedIn && !thread.locked && (
+                        <button
+                          className="flex items-center bg-gray-950 float right"
+                          onClick={() => toggleCheck(comment.id)}
+                        >
+                          {comment.isAnswer ? 'Remove answer' : 'Mark as answer'}
+                        </button>
+                      )}
+                    </p>
+                    <p className="text-gray-400 text-xs">
+                      Replies: {comment.replies ? comment.replies.length : 0}
+                    </p>
+                    <p className="text-gray-400 text-xs">Likes: {comment.likes || 0}</p>
+
+                    {/* Replies */}
+                    <div className="ml-6 mt-2 border-l-2 pl-4 border-gray-300">
+                      {comment.replies &&
+                        comment.replies.map((reply) => (
+                          <div key={reply.id} className="ml-2 mt-2 bg-gray-100 rounded p-2 shadow-md">
+                            <p className="text-gray-900">{reply.content}</p>
+                            <p className="text-gray-500 text-xs">- {reply.creator.userName}</p>
+                          </div>
+                        ))}
+                    </div>
+
+                    {/* Reply textarea and button */}
+                    {isLoggedIn && !thread.locked && (
+                      <div className="mt-3 ml-6">
+                        <textarea
+                          className="w-full p-2 bg-gray-800 text-white rounded-lg"
+                          placeholder="Write your reply..."
+                          value={reply[comment.id] || ""}
+                          onChange={(e) => setReply({ ...reply, [comment.id]: e.target.value })}
+                        />
+                        <button
+                          className="mt-2 bg-emerald-500 text-white px-3 py-1 rounded-md hover:bg-emerald-700"
+                          onClick={() => handleReply(comment.id)}
+                        >
+                          Reply
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                ))}
+                <hr className="mt-7" />
+
+                {/* Comment Input */}
+                {isLoggedIn && !thread.locked && (
+                  <div className="relative flex items-center right-5 mt-2 w-[600px]">
+                    <textarea
+                      className="text-white m-4 p-5 rounded-md w-[800px] h-[80px] border  bg-gray-800"
+                      placeholder="Write your comment..."
+                      value={comment}
+                      onChange={(e) => setComment(e.target.value)}
+                    />
+                    <button
+                      className="text-emerald-500 rounded-md px-5 w-13 h-[50px] hover:bg-emerald-800 bg-white absolute right-9"
+                      onClick={handleAddComment}
+                      title={!user ? "You must be logged in" : ""}
+                    >
+                      Comment
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
